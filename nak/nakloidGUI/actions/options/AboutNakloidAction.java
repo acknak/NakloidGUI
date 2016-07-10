ea260071b7a52cab40ac450968173fa1eeee2392
acknak <acknak39@gmail.com>
2016-07-10 16:46:07 +0900

    version up

--- nak/nakloidGUI/actions/options/AboutNakloidAction.java
+++ nak/nakloidGUI/actions/options/AboutNakloidAction.java
@@ -14,16 +14,28 @@ public class AboutNakloidAction extends AbstractAction {
 
 	@Override
 	public void run() {
-		 MessageDialog.openInformation(mainWindow.getShell(), "Nakloidについて", "Nakloid (GUI) ver.160429\n"
+		 MessageDialog.openInformation(mainWindow.getShell(), "Nakloidについて", "Nakloid (GUI) ver.160710\n"
 		 	+ "https://github.com/acknak/Nakloid/\n\n"
 		 	+ "this software includes the work that is distributed in the Apache License 2.0\n\n"
 		 	+ "Library List (Nakloid)\n"
 		 	+ "・Boost C++ Libraries\n"
+		 	+ "  http://www.boost.org/\n"
 		 	+ "・FFTSS\n"
-		 	+ "・RapidJSON\n\n"
+		 	+ "  http://www.ssisc.org/fftss/\n"
+		 	+ "・RapidJSON\n"
+		 	+ "  http://rapidjson.org/\n\n"
+		 	+ "Library List (WORLD for Nakloid)\n"
+		 	+ "・WORLD\n"
+		 	+ "  http://ml.cs.yamanashi.ac.jp/world/\n"
+		 	+ "・Boost C++ Libraries\n"
+		 	+ "  http://www.boost.org/\n"
+		 	+ "・RapidJSON\n"
+		 	+ "  http://rapidjson.org/\n\n"
 		 	+ "Library List (NakloidGUI)\n"
 		 	+ "・SWT/JFace\n"
-		 	+ "・Jackson\n\n"
+		 	+ "  https://www.eclipse.org/swt/\n"
+		 	+ "・Jackson\n"
+		 	+ "  https://github.com/FasterXML/jackson\n\n"
 		 	+ "バグ報告・感想等は acknak39@gmail.com か @acknak39 まで");
 	}
 }
