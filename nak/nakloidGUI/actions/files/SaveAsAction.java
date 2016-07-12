7294bcf72884ed5d0691b4277ec4e4a2dab7e885
acknak <acknak39@gmail.com>
2016-07-12 23:32:12 +0900

    improve save_as action

--- nak/nakloidGUI/actions/files/SaveAsAction.java
+++ nak/nakloidGUI/actions/files/SaveAsAction.java
@@ -1,7 +1,9 @@
 package nak.nakloidGUI.actions.files;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 
@@ -25,9 +27,16 @@ public class SaveAsAction extends AbstractAction {
 		FileDialog openDialog = new FileDialog(mainWindow.getShell(), SWT.SAVE);
 		openDialog.setFilterExtensions(ext);
 		openDialog.setFilterNames(filterNames);
-		String strPath = openDialog.open();
-		if (strPath==null || strPath.isEmpty()) {
-			return;
+		String strPath;
+		while (true) {
+			strPath = openDialog.open();
+			if (strPath==null || strPath.isEmpty()) {
+				return;
+			}
+			File tmpFile = new File(strPath);
+			if (!tmpFile.exists() || MessageDialog.openConfirm(mainWindow.getShell(), "NakloidGUI", "選択されたファイルは既に存在します。上書きしますか？")) {
+				break;
+			}
 		}
 		NakloidGUI.preferenceStore.setValue("workspace.path_nar", strPath);
 		try {
