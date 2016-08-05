a93c0514e3bb3a79af33219d446962db431647df
acknak <acknak39@gmail.com>
2016-08-06 01:05:32 +0900

    improve vocal export dialog

--- nak/nakloidGUI/actions/files/ExportVocalAction.java
+++ nak/nakloidGUI/actions/files/ExportVocalAction.java
@@ -41,9 +41,16 @@ public class ExportVocalAction extends AbstractAction {
 		FileDialog openDialog = new FileDialog(mainWindow.getShell(), SWT.SAVE);
 		openDialog.setFilterExtensions(ext);
 		openDialog.setFilterNames(filterNames);
-		String strPath = openDialog.open();
-		if (strPath==null || strPath.isEmpty()) {
-			return;
+		String strPath;
+		while (true) {
+			strPath = openDialog.open();
+			if (strPath==null || strPath.isEmpty()) {
+				return;
+			}
+			File tmpFile = new File(strPath);
+			if (!tmpFile.exists() || MessageDialog.openConfirm(mainWindow.getShell(), "NakloidGUI", "選択されたファイルは既に存在します。上書きしますか？")) {
+				break;
+			}
 		}
 		Path pathExportVocal = Paths.get(strPath);
 
